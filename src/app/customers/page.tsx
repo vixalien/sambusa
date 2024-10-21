9fb5f61cd15cb420f5a5c3f618c7da7c35056706
Angelo Verlain <geoangercola@gmail.com>
2024-10-21 14:32:23 +0200

    fix customers page

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import {
-  Badge,
+  Card,
+  FooterHelp,
   IndexTable,
-  LegacyCard,
   Page,
   Text,
   useIndexResourceState,
 } from "@shopify/polaris";
+import customersAll from "~/data/customers.json";
+
+const customers = customersAll.slice(0, 100);
 
 export default function Home() {
   return (
@@ -18,92 +21,73 @@ export default function Home() {
 }
 
 function SimpleIndexTableExample() {
-  const orders = [
-    {
-      id: "1020",
-      order: "#1020",
-      date: "Jul 20 at 4:34pm",
-      customer: "Jaydon Stanton",
-      total: "$969.44",
-      paymentStatus: <Badge progress="complete">Paid</Badge>,
-      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
-    },
-    {
-      id: "1019",
-      order: "#1019",
-      date: "Jul 20 at 3:46pm",
-      customer: "Ruben Westerfelt",
-      total: "$701.19",
-      paymentStatus: <Badge progress="partiallyComplete">Partially paid</Badge>,
-      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
-    },
-    {
-      id: "1018",
-      order: "#1018",
-      date: "Jul 20 at 3.44pm",
-      customer: "Leo Carder",
-      total: "$798.24",
-      paymentStatus: <Badge progress="complete">Paid</Badge>,
-      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
-    },
-  ];
   const resourceName = {
-    singular: "order",
-    plural: "orders",
+    singular: "customer",
+    plural: "customers",
   };
 
   const { selectedResources, allResourcesSelected, handleSelectionChange } =
-    useIndexResourceState(orders);
+    useIndexResourceState(customers);
 
-  const rowMarkup = orders.map(
+  const rowMarkup = customers.map(
     (
-      { id, order, date, customer, total, paymentStatus, fulfillmentStatus },
+      { id, email, last_activity, name, signup_date },
       index,
     ) => (
       <IndexTable.Row
-        id={id}
+        id={id.toString()}
         key={id}
-        selected={selectedResources.includes(id)}
+        selected={selectedResources.includes(id.toString())}
         position={index}
       >
         <IndexTable.Cell>
-          <Text variant="bodyMd" fontWeight="bold" as="span">
-            {order}
+          <Text as="span" alignment="end" numeric>
+            {id}
           </Text>
         </IndexTable.Cell>
-        <IndexTable.Cell>{date}</IndexTable.Cell>
-        <IndexTable.Cell>{customer}</IndexTable.Cell>
+        <IndexTable.Cell>
+          <Text variant="bodyMd" fontWeight="bold" as="span">
+            {name}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{email}</IndexTable.Cell>
         <IndexTable.Cell>
           <Text as="span" alignment="end" numeric>
-            {total}
+            {signup_date}
           </Text>
         </IndexTable.Cell>
-        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
-        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
+        <IndexTable.Cell>
+          <Text as="span" alignment="end" numeric>
+            {last_activity}
+          </Text>
+        </IndexTable.Cell>
       </IndexTable.Row>
     ),
   );
 
   return (
-    <LegacyCard>
-      <IndexTable
-        resourceName={resourceName}
-        itemCount={orders.length}
-        selectedItemsCount={allResourcesSelected
-          ? "All"
-          : selectedResources.length}
-        onSelectionChange={handleSelectionChange}
-        headings={[
-          { title: "Order" },
-          { title: "Date" },
-          { title: "Customer" },
-          { title: "Total", alignment: "end" },
-          { title: "Payment status" },
-          { title: "Fulfillment status" },
-        ]}
-      >
-        {rowMarkup}
-      </IndexTable>
-    </LegacyCard>
+    <>
+      <Card padding="0">
+        <IndexTable
+          resourceName={resourceName}
+          itemCount={10}
+          selectedItemsCount={allResourcesSelected
+            ? "All"
+            : selectedResources.length}
+          onSelectionChange={handleSelectionChange}
+          headings={[
+            { title: "ID" },
+            { title: "Name" },
+            { title: "Email" },
+            { title: "Signup Date", alignment: "end" },
+            { title: "Last Activity", alignment: "end" },
+          ]}
+          hasZebraStriping
+        >
+          {rowMarkup}
+        </IndexTable>
+      </Card>
+      <FooterHelp />
+    </>
   );
 }
